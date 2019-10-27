b020f2a360115c01d1e176998a4eedb1d8ccc659
Johann Rosain <johann.rosain@gmail.com>
2019-10-27 14:52:36 +0100

    Added movement
    - Added player movement with arrows / zqsd
    - Added DontDestroyOnLoad which will take the settings of the player
    - Moved .gitignore to the good folder

---
+++ survival-horror-game/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dd = FindObjectOfType<DontDestroyOnLoad>();
+    }
+
+    void Update()
+    {
+        int x = 0;
+        int y = 0;
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(_dd.LeftKey()))
+        {
+            x -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(_dd.RightKey()))
+        {
+            x += 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(_dd.UpKey()))
+        {
+            y += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(_dd.DownKey()))
+        {
+            y -= 1;
+        }
+        Movement(x, y);
+    }
+    /// <summary>
+    /// Manages player's movement
+    /// </summary>
+    private void Movement(float x, float y)
+    {
+        Vector2 direction = new Vector2(x, y).normalized;
+        Vector2 pos = transform.position;
+        pos += direction * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    // Defines the walking speed of the player
+    public float speed;
+    // Player settings
+    private DontDestroyOnLoad _dd;
+}
---
+++ survival-horror-game/Assets/Scripts/Manager/DontDestroyOnLoad.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    void Awake()
+    {
+        left = KeyCode.Q;
+        right = KeyCode.D;
+        up = KeyCode.Z;
+        down = KeyCode.S;
+
+        DontDestroyOnLoad(gameObject);
+    }
+    public KeyCode LeftKey()
+    {
+        return left;
+    }
+    public KeyCode RightKey()
+    {
+        return right;
+    }
+    public KeyCode UpKey()
+    {
+        return up;
+    }
+    public KeyCode DownKey()
+    {
+        return down;
+    }
+
+    private KeyCode left;
+    private KeyCode right;
+    private KeyCode up;
+    private KeyCode down;
+}
