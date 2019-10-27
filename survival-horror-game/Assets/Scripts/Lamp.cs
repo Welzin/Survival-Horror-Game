ab8c57ddb070a6813bad2ac2acb8c2a8fa8a9c17
Huftier Benoît <ephesielpgmail.com>
2019-10-27 14:52:17 +0100

    Ajout de la librairie de Light2D

---
+++ survival-horror-game/Assets/Scripts/Lamp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    private void Start()
+    {
+        Active = false;
+    }
+
+    public bool Active
+    {
+        get
+        {
+            return _active;
+        }
+        set
+        {
+            if (GetComponent<MeshRenderer>())
+            {
+                MeshRenderer mesh = GetComponent<MeshRenderer>();
+                _active = value;
+                mesh.enabled = _active;
+            }
+        }
+    }
+
+    private bool _active;
+}
