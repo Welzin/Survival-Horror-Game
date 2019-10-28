ab92178be10aed1aa360f2613f65d7155d1b95d2
Johann Rosain <johann.rosain@gmail.com>
2019-10-28 13:57:19 +0100

    Added Monsters
    - Pattern class : takes an action, an interval, and an amplitude
    - Monsters can move according to set pattern (in the editor)
    - Currently can't follow the player or detect sound / light

---
+++ survival-horror-game/Assets/Scripts/Monsters/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pattern
+{
+    public Actions action;
+    public int intervalUntilNextAction;
+    public int movementAmplitude;
+
+    public Pattern(Actions act, int interval, int movement)
+    {
+        action = act;
+        intervalUntilNextAction = interval;
+        movementAmplitude = movement;
+    }
+
+    public Pattern() { }
+}
