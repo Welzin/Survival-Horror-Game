7d250c9a7b1f13e8b551148e731f06cb37adcac9
Johann Rosain <johann.rosain@gmail.com>
2019-10-28 13:57:19 +0100

    Added Monsters
    - Pattern class : takes an action, an interval, and an amplitude
    - Monsters can move according to set pattern (in the editor)
    - Currently can't follow the player or detect sound / light

--- survival-horror-game/Assets/Scripts/Monsters/Monster.cs
+++ survival-horror-game/Assets/Scripts/Monsters/Monster.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 // The monster will move according to these action
 // Each monster will move the same amount of time
 // But their speed is different.
+<<<<<<< HEAD
 // More actions can be added later
+=======
+>>>>>>> Added Monsters
 public enum Actions
 {
     Up,
