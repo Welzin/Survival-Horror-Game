6478407da20b91f69d5c7aabd630fe287992fe4b
Johann Rosain <johann.rosain@gmail.com>
2019-10-29 12:22:05 +0100

    Sound added
    - The player can emit sound when he walks / runs
    - The monster will get the sound
    - If the monster doesn't have any target, and hears the sound, it will
    target the sound
    - If it has a target, it'll check which target is closer, and go toward
    the closer one

--- survival-horror-game/Assets/Scripts/Manager/SoundManager.cs
+++ survival-horror-game/Assets/Scripts/Manager/SoundManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private void Start()
+    {
+        _listeners = new List<Monster>();
+    }
     public void Subscribe(Monster monster)
     {
         _listeners.Add(monster);
@@ -11,10 +15,29 @@ public class SoundManager : MonoBehaviour
 
     public void CreateSoundWave(Vector2 center, float radius)
     {
-        foreach(Monster m in _listeners)
+        GameObject go = CreateGO(center, radius);
+        if(_listeners.Count != 0)
         {
-            m.DetectSound(center, radius);
+            foreach (Monster m in _listeners)
+            {
+                m.DetectSound();
+            }
         }
+        // Delete sound wave
+        Destroy(go);
+    }
+
+    private GameObject CreateGO(Vector2 center, float radius)
+    {
+        GameObject go = new GameObject();
+        go.transform.position = center;
+        CircleCollider2D sound = go.AddComponent<CircleCollider2D>(); 
+        sound.radius = radius;
+        sound.isTrigger = true;
+        // Sound layer
+        go.layer = 12;
+        go.name = "Soundwave";
+        return go;
     }
 
     // All monsters which listen to any sound of the player / objects launched by the player
