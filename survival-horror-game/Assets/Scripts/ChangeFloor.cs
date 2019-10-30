4dea38db4551fa326adf55bb1316dd3a8ce3cf86
Huftier Benoît <ephesielpgmail.com>
2019-10-30 13:34:52 +0100

    Tutorial house colliders and wall finished

---
+++ survival-horror-game/Assets/Scripts/ChangeFloor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFloor : MonoBehaviour
+{
+    public List<GameObject> toShow;
+    public List<GameObject> toHide;
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            foreach (GameObject go in toShow)
+            {
+                go.SetActive(true);
+            }
+            foreach (GameObject go in toHide)
+            {
+                go.SetActive(false);
+            }
+        }
+    }
+}
