a4f24c2030b1576d0260c58a20971d4265c8c78a
Johann Rosain <johann.rosain@gmail.com>
2019-11-01 18:20:49 +0100

    Home scene began
    - Home scene got 3 buttons : play, options and quit
    - When playing, the tutorial is launched
    - When quitting, the user will exit the app (the game mode if on unity)
    - When pushing options, the "options" screen will be displayed

---
+++ survival-horror-game/Assets/Scripts/UI/HomeButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeButtons : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ShowOptions()
+    {
+
+        home.SetActive(false);
+        options.SetActive(true);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public string scene;
+    public GameObject options;
+    public GameObject home;
+}
