066d5507ba7f8eb6046cfd05b6612b5e5e247b05
Huftier Benoît <ephesielpgmail.com>
2019-11-09 09:41:46 +0100

    Add a method to see door between nodes

--- survival-horror-game/Assets/Scripts/Monsters/Node.cs
+++ survival-horror-game/Assets/Scripts/Monsters/Node.cs
@@ -8,6 +8,7 @@ public class Node : MonoBehaviour
     {
         _node = gameObject;
     }
+
     public float X() => _node.transform.position.x;
     public float Y() => _node.transform.position.y;
     public Vector2 Position() => _node.transform.position;
@@ -25,5 +26,19 @@ public class Node : MonoBehaviour
                 node.neighbours.Add(this);
             }
         }
+    }
+
+    public Door DoorBetweenNodes(Node other)
+    {
+        Door door = null;
+        Vector2 distance = other.Position() - Position();
+
+        RaycastHit2D hit = Physics2D.Raycast(Position(), distance.normalized, distance.magnitude, LayerMask.GetMask("Door"));
+        if (hit.collider != null && hit.collider.gameObject.GetComponent<Door>())
+        {
+            door = hit.collider.gameObject.GetComponent<Door>();
+        }
+
+        return door;
     }
 }
