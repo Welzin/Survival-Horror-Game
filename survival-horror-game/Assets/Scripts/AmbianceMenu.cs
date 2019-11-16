5eee8bfcd420cdb63fcd57064dfcdd6a47004e91
Huftier Benoît <ephesielpgmail.com>
2019-11-16 17:49:22 +0100

    Add background in menu

---
+++ survival-horror-game/Assets/Scripts/AmbianceMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbianceMenu : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Lightning());
+    }
+    
+    private IEnumerator Lightning()
+    {
+        yield return new WaitForSeconds(1);
+        thunder.StartLoopStrike(8, 12);
+    }
+
+    public Thunder thunder;
+}
